593124ec3dc301bc9cd371e4f6278d5148d6797f
Simona <shargasimona@gmail.com>
2024-07-10 22:37:13 +0300

    drivers test commented

--- tests/drivers.spec.ts
+++ tests/drivers.spec.ts
@@ -1,19 +1,22 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../src/pages/login.page';
-import { goto } from '../src/navigation';
-import { DriversPage } from '../src/pages/drivers.page';
-import { users } from '../src/users';
+// import { test, expect } from '@playwright/test';
+// import { LoginPage } from '../src/pages/login.page';
+// import { goto } from '../src/navigation';
+// import { DriversPage } from '../src/pages/drivers.page';
+// import { users } from '../src/users';
 
-test('Drivers page test', async ({ page }) => {
-    test.beforeEach( async ({ page }) => {
-      const loginPage = new LoginPage(page);
-      await goto(loginPage);
-      await loginPage.login(users.testUser);
+// test.describe ('Drivers page test', () => {
+//     test.beforeEach( async ({ page }) => {
+//       const loginPage = new LoginPage(page);
+//       await goto(loginPage);
+//       await loginPage.login(users.testUser);
     
-    })
+//     })
     
-    const driversPage = new DriversPage(page);
+//     test('Chevrone test', async ({ page }) => {
+//       const driversPage = new DriversPage(page);
     
-    await goto(driversPage);
-    await driversPage.header.chevron.click();
-  });
+//     await goto(driversPage);
+//     await driversPage.header.chevron.click();
+//     });
+    
+//   });
